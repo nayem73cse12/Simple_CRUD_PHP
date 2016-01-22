a836da2623eedd553a5a86ce84d785cf52c6f2dc
Abu Hanife <simplenayem@gmail.com>
2016-01-23 00:02:55 +0600

    Bootstrap

---
+++ Student_Entry.php
@@ -0,0 +1,11 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Entry a new student</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
